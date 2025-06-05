f766efe16a440f79a6807ae308e8f40981b8d2ab
jay-sign <jaysinghdulrasar@gmail.com>
2025-06-05 16:29:26 +0530

    Kaggle api test

--- app.py
+++ app.py
@@ -32,3 +32,8 @@ def list_kaggle_files(owner, dataset):
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
+if __name__ == '__main__':
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
